88d4a90ff4f08ef567d0a58e935448343d6b9eae
Matt Taylor <twenty7@gmail.com>
2025-03-06 17:53:16 -0800

    Cleanup customized port issue

--- apps/login/src/app/login/route.ts
+++ apps/login/src/app/login/route.ts
@@ -401,8 +401,6 @@ export async function GET(request: NextRequest) {
     } else {
       const loginNameUrl = constructUrl(request, "/loginname");
 
-      // const loginNameUrl = new URL("/loginname", request.url);
-      // loginNameUrl.port = '';
       loginNameUrl.searchParams.set("requestId", requestId);
       if (authRequest?.loginHint) {
         loginNameUrl.searchParams.set("loginName", authRequest.loginHint);
