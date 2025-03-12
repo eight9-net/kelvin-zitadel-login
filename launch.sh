da9c3d3f7a92ccc4b06486e7a2d283967be083c4
Matt Taylor <twenty7@gmail.com>
2025-03-12 16:24:50 -0700

    Fix .env.local

--- launch.sh
+++ launch.sh
@@ -3,15 +3,17 @@
 echo "HOSTNAME=${HOSTNAME}"
 echo "PORT=${PORT}"
 echo "ZITADEL_API_URL: ${ZITADEL_API_URL}"
+echo "ZITADEL_SERVICE_USER_ID: ${ZITADEL_SERVICE_USER_ID}"
 
 # Write .env.local
 if [ -d /app/apps/login/ ]; then
-  touch /app/apps/login/.env.local
-  echo "EMAIL_VERIFICATION=false" >> /app/apps/login/.env.local
+  echo "EMAIL_VERIFICATION=false" > /app/apps/login/.env.local
   echo "DEBUG=true" >> /app/apps/login/.env.local
   echo "ZITADEL_API_URL=${ZITADEL_API_URL}" >> /app/apps/login/.env.local
-  echo "ZITADEL_SERVICE_USER_ID=${ZITADEL_SERVICE_USER_ID}"  >> /app/apps/login/.env.local
-  echo "ZITADEL_SERVICE_USER_TOKEN=\"${ZITADEL_SERVICE_USER_TOKEN}\"" >> /app/apps/login/.env.local
+  echo "ZITADEL_SERVICE_USER_ID=${KAUTH_UI_SERVICE_USER}"  >> /app/apps/login/.env.local
+  echo "ZITADEL_SERVICE_USER_TOKEN=\"${KAUTH_UI_SERVICE_TOKEN}\"" >> /app/apps/login/.env.local
 fi
+
+cat /app/apps/login/.env.local
 
 pnpm start:built -- --port=$PORT --hostname=$HOSTNAME
