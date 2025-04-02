1128700825cf88d143ae3e578f19f6c776bd0bff
Matt Taylor <twenty7@gmail.com>
2025-04-01 17:36:31 -0700

    Add loginName to User Nav Links. Change to client side component.

--- apps/login/src/components/user-nav-links.tsx
+++ apps/login/src/components/user-nav-links.tsx
@@ -1,43 +1,60 @@
+"use client";
+
 import { HomeIcon, ArrowRightStartOnRectangleIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
-import { getMostRecentSessionCookie, getAllSessionCookieIds } from "@/lib/cookies";
+import { getMostRecentCookieWithLoginname } from "@/lib/cookies";
+import { useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
 
 
+export const UserNavLinks = () => {
+  let [session, setSession] = useState<any>([]);
+  const searchParams = useSearchParams();
+  const loginName = searchParams.get("loginName");
 
-export const UserNavLinks = async () => {
-  let session = null;
-  let session_ids = [];
-  try {
-    session_ids = await getAllSessionCookieIds();
-    if (session_ids.length > 0) {
-      session = await getMostRecentSessionCookie();
-    }
-  } catch (error) {
-    console.error(error);
-  }
+  useEffect(() => {
+    const loadCookies = async () => {
+      if (loginName) {
+        try {
+          const result = await getMostRecentCookieWithLoginname({ loginName });
+          setSession(result);
+        } catch (e) {
+          console.error('Err', e)
+        }
+      }
+    };
+
+    loadCookies();
+  }, []);
 
   if (!session || !session.loginName) {
     return undefined;
   }
 
+  let dashboard_link = "/dashboard";
+  if (loginName) {
+    dashboard_link += `?loginName=${loginName}`;
+  }
+
   return (
     <div>
-
-      <a
-        href="/dashboard"
+      <Link
+        prefetch={false}
+        href={dashboard_link}
         className="inline-flex space-x-2 rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium text-gray-100 hover:bg-gray-500 hover:text-white mx-2"
       >
         <HomeIcon className="block w-4" />
         <div>Dashboard</div>
-      </a>
+      </Link>
 
-      <a
+      <Link
+        prefetch={false}
         href="/logout"
         className="inline-flex space-x-2 rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium text-gray-100 hover:bg-gray-500 hover:text-white mx-2"
       >
         <ArrowRightStartOnRectangleIcon className="block w-4" />
         <div>Logout</div>
-      </a>
+      </Link>
 
     </div>
   );
